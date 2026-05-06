802a451f46b7d381e0bda67b4572bfe1315ee2f2
ganeshfromindia <ganeshghate5@gmail.com>
2026-05-06 16:26:45 +0530

    url api

--- util/config.tsx
+++ util/config.tsx
@@ -1,8 +1,5 @@
 export const MAIN_URL = "https://api-infoportal-backend.onrender.com";
-// export const MAIN_URL = "https://9f9c-115-98-233-83.ngrok-free.app";
 // export const MAIN_URL = "http://localhost:5000";
-// export const MAIN_URL = "https://a168-115-98-234-172.ngrok-free.app";
 // export const MAIN_URL = "https://api-infoportal-backend-15zr.onrender.com";
-// export const MAIN_URL = "https://61f5-115-98-235-221.ngrok-free.app";
 // export const MAIN_URL = "https://heavy-ravens-shop.loca.lt";
 // export const MAIN_URL = "http://api.infoportal.co.in/";
